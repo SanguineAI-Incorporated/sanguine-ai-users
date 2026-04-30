8010ee550e1b88a3052aeafb97ddb376b4b18ea1
Evana Moon <evan.moon@icloud.com>
2026-05-01 06:13:15 +1000

    Add Documentation route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard.jsx";
 import EngineEditor from "./pages/EngineEditor.jsx";
+import Documentation from "./pages/Documentation";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/engine-editor" element={<EngineEditor />} />
+      <Route path="/documentation" element={<Documentation />} />
     </Routes>
   );
 }
