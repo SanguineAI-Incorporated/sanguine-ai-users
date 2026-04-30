8e8e46f0c43d56eeb164f958f5dd4fe24c71bbae
Evana Moon <evan.moon@icloud.com>
2026-05-01 06:04:49 +1000

    Add Input component with forwardRef support

---
+++ src/components/ui/input.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Input = React.forwardRef(({ className = "", type = "text", ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      ref={ref}
+      className={
+        "flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm " +
+        "focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 " +
+        className
+      }
+      {...props}
+    />
+  );
+});
+
+Input.displayName = "Input";
+
+export { Input };
